5da6f4490db64483519a3dc95f8e0503ed9e9556
Bill <wspreitzer2@comcast.net>
2023-08-27 17:16:49 -0500

    Implemented the Complete Button

--- app1/gui.py
+++ app1/gui.py
@@ -8,6 +8,7 @@ input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 exit_button = sg.Button("Exit")
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 list_box = sg.Listbox(values=todos,
                       key="todos",
                       enable_events=True,
@@ -16,7 +17,7 @@ window = sg.Window("My To-Do App",
                    layout=[
                        [label],
                        [input_box, add_button],
-                       [list_box, edit_button],
+                       [list_box, edit_button, complete_button],
                        [exit_button]],
                    font=("Helvetica", 20))
 while True:
@@ -40,6 +41,12 @@ while True:
             todos[index] = new_todo
             write_todos(todos)
             window["todos"].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos.remove(todo_to_complete)
+            write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case "Exit" | sg.WIN_CLOSED:
