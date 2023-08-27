2f7415b0a7185efa7b104954663075030830dc88
Bill <wspreitzer2@comcast.net>
2023-08-27 13:39:02 -0500

    Implemented add and exit buttons

--- app1/gui.py
+++ app1/gui.py
@@ -1,9 +1,26 @@
 from functions import get_todos, write_todos
 import PySimpleGUI as sg
 
+todos = get_todos()
+enter = True
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
-add_button = sg.Button('add')
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
+exit_button = sg.Button("Exit")
+window = sg.Window("My To-Do App",
+                   layout=[
+                       [label],
+                       [input_box, add_button],
+                       [exit_button] ],
+                   font=("Helvetica", 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos.append(values["todo"] + "\n")
+            write_todos(todos)
+        case "Exit" | sg.WIN_CLOSED:
+            break
 window.close()
