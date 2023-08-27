a1da6557aba3ba48e8fe4584b88760c541f6530e
Bill <wspreitzer2@comcast.net>
2023-08-27 17:26:10 -0500

    Added some error handling

--- app1/gui.py
+++ app1/gui.py
@@ -31,22 +31,29 @@ while True:
             window["todos"].update(values=todos)
             window["todo"].update(value="")
         case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"]
-            if new_todo.endswith("\n"):
-                new_todo = new_todo
-            else:
-                new_todo = new_todo + "\n"
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            write_todos(todos)
-            window["todos"].update(values=todos)
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"]
+                if new_todo.endswith("\n"):
+                    new_todo = new_todo
+                else:
+                    new_todo = new_todo + "\n"
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                write_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
+
         case "Complete":
-            todo_to_complete = values["todos"][0]
-            todos.remove(todo_to_complete)
-            write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
+            try:
+                todo_to_complete = values["todos"][0]
+                todos.remove(todo_to_complete)
+                write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case "Exit" | sg.WIN_CLOSED:
